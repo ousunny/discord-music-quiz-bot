616f3246fd1d4c8cb2a0394ebd9ddafdc5f99eec
Sunny <ousunny91@gmail.com>
2020-12-08 21:06:36 -0500

    Add set command for setting channels

---
+++ src/commands/set.js
@@ -0,0 +1,28 @@
+module.exports = {
+    name: 'set',
+    description: 'Set settings for bot',
+    args: true,
+    execute(message, args) {
+        switch (args[0]) {
+            case 'voice':
+                message.client.voiceChannel = message.client.channels.cache.get(
+                    args[1]
+                );
+                return message.channel.send(
+                    `The voice channel has been set to ${message.client.channels.cache.get(
+                        args[1]
+                    )}`
+                );
+                break;
+            case 'text':
+                message.client.textChannel = message.client.channels.cache.get(
+                    args[1]
+                );
+                return message.channel.send(
+                    `The text channel has been set to ${message.client.channels.cache.get(
+                        args[1]
+                    )}`
+                );
+        }
+    },
+};
