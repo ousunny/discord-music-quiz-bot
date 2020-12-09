2966fe1e2dbbd1b4566d2f60d2026726669deb05
Sunny <ousunny91@gmail.com>
2020-12-09 16:07:48 -0500

    Add message for player to also join the voice channel

--- src/commands/join.js
+++ src/commands/join.js
@@ -4,11 +4,21 @@ module.exports = {
     execute(message, args) {
         if (message.client.answer) return;
 
+        if (!message.client.voiceChannel)
+            return message.channel.send('Please set the voice channel first');
+
         if (message.client.players.includes(message.author))
             return message.reply('You already joined the game!');
 
         message.client.players.push(message.author);
 
-        return message.reply('You have joined the game!');
+        message.reply('You have joined the game!');
+
+        if (!message.member.voice.channel)
+            message.reply(
+                `Please join the voice channel ${message.client.voiceChannel}`
+            );
+
+        return;
     },
 };
